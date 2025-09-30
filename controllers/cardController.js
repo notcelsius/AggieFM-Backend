b43855780bbcce773560aa7b85fb53c30141c84c
unknown <ktworkandedu@gmail.com>
2025-09-30 16:42:48 -0700

    Added async handler and handling for get and post on the contact controller

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,20 +1,22 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc Get all cards
 //@route GET /api/cards
 //@access public
-const getCard = (req, res) => {
+const getCard = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get card"})
-}
+})
 
 //@desc Create new card
 //@route POST /api/cards
 //@access public
-const createCard = (req, res) => {
+const createCard = asyncHandler(async (req, res) => {
     const {lat, lng, spotifyTrackID, caption} = req.body
     if (!lat || !lng || !spotifyTrackID || !caption) {
         res.status(400)
         throw new Error("All fields are mandatory")
     }
     res.status(201).json({ message: "Post card"})
-}
+})
 
 module.exports = {getCard, createCard}
