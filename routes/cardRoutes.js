91b7fb55972896602ca9cdbc56845f5101f07994
unknown <ktworkandedu@gmail.com>
2025-09-30 01:36:17 -0700

    Added basic get and post  endpoints for cards

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
+router.route("/").get((req, res) => {
+    res.status(200).json({ message: "Get card"})
+})
 
+router.route("/").post((req, res) => {
+    res.status(200).json({ message: "Post card"})
+})
 
 module.exports = router
