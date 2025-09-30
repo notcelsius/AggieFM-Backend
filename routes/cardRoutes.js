1ebc6ba02e337d32b342b09a8bc3e20812e49127
unknown <ktworkandedu@gmail.com>
2025-09-30 01:48:24 -0700

    Made code look cleaner

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -2,8 +2,6 @@ const express = require("express")
 const router = express.Router()
 const { getCard, createCard } = require("../controllers/cardController")
 
-router.route("/").get(getCard)
-
-router.route("/").post(createCard)
+router.route("/").get(getCard).post(createCard)
 
 module.exports = router
