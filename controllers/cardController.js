49892d20913897f38b28cc04e85f6815fd964940
unknown <ktworkandedu@gmail.com>
2025-10-04 21:30:07 -0700

    Made postCard to return the newly created card

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -13,14 +13,14 @@ const getAllCards = asyncHandler(async (req, res) => {
 //@route POST /api/cards
 //@access public
 const postCard = asyncHandler(async (req, res) => {
-    const userId = 1
+    const userId = 1    // mock user for now
     const {lat, lng, spotifyTrackId, caption} = req.body
     if (!lat || !lng || !spotifyTrackId) {
         res.status(400)
         throw new Error("lat, lng, and spotifyTrackId are required")
     }
-    const card = await createCard(userId, lat, lng, spotifyTrackId, caption)
-    res.status(201).json({ message: "Post card"})
+    const card = await createCard({userId, lat, lng, spotifyTrackId, caption})
+    res.status(201).json(card)
 })
 
 module.exports = {getAllCards, postCard}
